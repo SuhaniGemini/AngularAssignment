ef61e9bcc8455a86c70df3200af4f9ab16b59508
Suhani Pachouri <Suhani.Pachouri@Geminisolutions.com>
2023-05-25 10:38:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  hide = false;
+  counter: number =0;
+  toggle() {
+    this.hide = !this.hide;
+    this.counter++;
+    console.log(this.counter);
+  }
+  
+  count:number =0;
+  fetch(a:string){
+    const heading = document.createElement("h3");
+    heading.innerText = a;
+    document.body.appendChild(heading)
+    this.count++;
+    if(this.count%5==0){
+      heading.style.backgroundColor = "blue",
+      heading.style.display="inline-block";
+    }
+  }
+  
 }
